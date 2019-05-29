d3d181a61f00f5bdac3b68baee16ace8f6118db3
Cailyn Hansen <cailyn.e.hansen@gmail.com>
2019-05-29 12:23:34 -0400

    Update data-dump-email script
    Make body text more descriptive; allow users to specify a from email (to avoid spam filter)

--- data-dump-email/data-dump-email.sh
+++ data-dump-email/data-dump-email.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-text="This is the data dump for $(date +%F)"
-subject="Mapping Violence Data Dump for $(date +%B)"
+text="Hi team,
+
+Attached you will find the export of the current data in the database. This is the data dump for $(date +%F). Please let me know if you have any questions. Reminder that this export of data includes the POIs that are still in the Draft Stage. \
+
+
+Best,"
+subject="Mapping Violence Data Export for $(date +%B)"
 email_file=data-dump-email/emails.txt
 body_path="/tmp/usr/script/mailx"
 body_file="$body_path""/body_file.txt"
@@ -13,6 +18,9 @@ if [ ! -d "$body_path" ]; then
     mkdir -p "$body_path";
 fi
 
+echo -n "Please enter your Brown email: "
+read -e sender_email
+
 echo "$text" > $body_file
 
 docker exec -it "$docker_container" sh -c "mkdir -p /usr/src/scripts && touch $json_file"
@@ -20,4 +28,4 @@ docker cp data-dump-email/data-extraction.js "$docker_container":"$docker_script
 docker exec -it "$docker_container" sh -c "mongo --quiet $docker_script_file > $json_file"
 docker cp "$docker_container":$json_file data-dump-email/data.json
 
-mailx -s "$subject" -a data-dump-email/data.json $(awk '{ line = $1 " " line }; END { print line; }' $email_file) < $body_file
+mailx -s "$subject" -r "$sender_email" -a data-dump-email/data.json $(awk '{ line = $1 " " line }; END { print line; }' $email_file) < $body_file
